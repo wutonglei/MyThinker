b2c25582bb0a482d58f8dc7b6485a08e91060685
wotonglei <616302953@qq.com>
2020-12-14 01:47:43 +0800

    随便改改

--- app/src/main/java/com/example/mythinker/MainActivity.java
+++ app/src/main/java/com/example/mythinker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mythinker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.btn_update);
+        Button iv=findViewById(R.id.btn_update);
+        Button text=findViewById(R.id.btn_update);
+
     }
 }
